36372a4947345d802fa9caa342ce1c38de03511e
RyosukeKamimura <come1234560@gmail.com>
2020-08-31 09:46:29 +0900

    Initial Commit

---
+++ Tracking_User_Face/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tracking_User_Face
+//
+//  Created by 神村亮佑 on 2020/08/31.
+//  Copyright © 2020 神村亮佑. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
